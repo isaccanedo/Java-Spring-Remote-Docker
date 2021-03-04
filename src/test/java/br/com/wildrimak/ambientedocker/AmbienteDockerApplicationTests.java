541618342ab280c19d687e211b69c8cd5bce7435
isaccanedo <isaccanedo@gmail.com>
2021-03-03 22:17:53 -0300

    :zap: Initial commit

---
+++ src/test/java/br/com/wildrimak/ambientedocker/AmbienteDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.isaccanedo.ambientedocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmbienteDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
